0ca4d8a58d646e620f7f95df44cf32ccdc497030
DominikMendoza <u20201b980@upc.edu.pe>
2023-05-11 15:47:05 -0500

    ViewModel

--- app/src/main/java/pe/edu/upc/appsuperzound/data/local/AlbumDatabase.kt
+++ app/src/main/java/pe/edu/upc/appsuperzound/data/local/AlbumDatabase.kt
@@ -5,7 +5,6 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import pe.edu.upc.appsuperzound.data.model.Album
-import pe.edu.upc.superherocompose.data.local.ProductDatabase
 
 @Database(entities = [Album::class], version = 1)
 abstract class AlbumDatabase: RoomDatabase() {
--- app/src/main/java/pe/edu/upc/appsuperzound/ui/screens/album/AlbumViewModel.kt
+++ app/src/main/java/pe/edu/upc/appsuperzound/ui/screens/album/AlbumViewModel.kt
@@ -1,2 +1,39 @@
 package pe.edu.upc.appsuperzound.ui.screens.album
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import pe.edu.upc.appsuperzound.data.local.AlbumDatabase
+import pe.edu.upc.appsuperzound.data.model.Album
+import pe.edu.upc.appsuperzound.data.remote.AlbumClient
+import pe.edu.upc.appsuperzound.repository.AlbumRepository
+
+class AlbumViewModel (application: Application) : AndroidViewModel(application) {
+    private val albumService = AlbumClient.albumService()
+    private val albumDao = AlbumDatabase.getInstance(application).albumDao()
+    private val albumRepository = AlbumRepository(albumDao, albumService)
+
+    private var _albums = albumRepository.albums
+    val albums get() = _albums
+
+    private var _favoriteAlbums = albumRepository.favoriteAlbums
+    val favoriteAlbums get() = _favoriteAlbums
+
+    fun fetchAlbums() {
+        albumRepository.fetchAlbums()
+        _albums.value = albumRepository.albums.value
+    }
+
+    fun fetchFavorites() {
+        albumRepository.fetchFavoriteAlbums()
+        _favoriteAlbums.value = albumRepository.favoriteAlbums.value
+    }
+
+    fun insert(album: Album) {
+        albumRepository.insert(album)
+    }
+
+    fun delete(album: Album) {
+        albumRepository.delete(album)
+        fetchFavorites()
+    }
+}
